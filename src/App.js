5dddc746468bcf3e3c3eba21f244085f41f8248a
bishalr0y <bishalroy895@gmail.com>
2021-05-08 20:31:32 +0530

    build product card layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Products from "./components/Products/Products";
 
 const App = () => {
     return(
         <div>
-            Hello world!
+            <Products />
         </div>
     )
     }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+
+const products = [
+    { id: 1, name: "Macbook Air", price: "Rs 98000", description: "Apple Macbook Air", image: "https://sm.mashable.com/t/mashable_in/review/m/macbook-ai/macbook-air-late-2020-review-the-one-with-apple-silicon-insi_2quc.960.jpg" },
+    { id: 2, name: "Nike Shoes", price: "Rs 8000", description: "Running Shoes", image: "https://media.gq-magazine.co.uk/photos/5eda54b0ed80d8890ee2ee4c/master/w_1920,c_limit/20200605-nike-06.jpg" }
+
+];
+
+const Products = () => {
+    return (<main>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    </main>)
+}
+
+export default Products;
