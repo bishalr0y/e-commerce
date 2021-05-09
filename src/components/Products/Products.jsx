60db77a6a3b845118e6090cbbdd20f07ebcd202c
bishalr0y <bishalroy895@gmail.com>
2021-05-09 19:47:08 +0530

    add navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from "./styles"
 
 const products = [
     { id: 1, name: "Macbook Air", price: "Rs 98000", description: "Apple Macbook Air", image: "https://sm.mashable.com/t/mashable_in/review/m/macbook-ai/macbook-air-late-2020-review-the-one-with-apple-silicon-insi_2quc.960.jpg" },
@@ -9,7 +10,9 @@ const products = [
 ];
 
 const Products = () => {
-    return (<main>
+    const classes = useStyles();
+    return (<main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
