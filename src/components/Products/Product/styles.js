ef0535b4b808e972c43317389ade63208f913406
bishalr0y <bishalroy895@gmail.com>
2021-05-10 18:22:05 +0530

    change the logo inside the navbar

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -7,7 +7,7 @@ export default makeStyles(() => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '66.25%', // 16:9
   },
   cartActions: {
     display: 'flex',
