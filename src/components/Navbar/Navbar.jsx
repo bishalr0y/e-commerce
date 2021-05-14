686709516b0c18f88a09dd9e3d336352a0d70c5e
bishalr0y <bishalroy895@gmail.com>
2021-05-14 11:18:00 +0530

    configure react router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import { AppBar, Toolbar, IconButton, Badge, Typography } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
 
 import logo from "../../assets/commerce.png";
 import useStyles from "./styles";
@@ -8,26 +9,26 @@ import useStyles from "./styles";
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
-
+    const location = useLocation();
 
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="E-Commerce" height="25px" className={classes.image} />
                         E-Commerce
                     </Typography>
                     <div className={classes.grow} />
-                    <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                    {location.pathname === "/" && (
+                        <div className={classes.button}>
+                            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
 
                                 <Badge badgeContent={totalItems} color="secondary">
                                     <ShoppingCart />
                                 </Badge>
-                        </IconButton>
-
-                    </div>
+                            </IconButton>
+                        </div>)}
                 </Toolbar>
             </AppBar>
         </>
