7773b47e1c04d84eada2324d97e703dd31642709
bishalr0y <bishalroy895@gmail.com>
2021-06-01 19:57:31 +0530

    fix active state count

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -7,7 +7,7 @@ import PaymentForm from '../PaymentForm';
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(2);
     const classes = useStyles();
 
     const Confirmation = () => (
@@ -25,7 +25,7 @@ const Checkout = () => {
             <main className={classes.layout} />
             <Paper className={classes.paper}>
                 <Typography variant="h4" align="center">Checkout</Typography>
-                <Stepper activeStep={0} className={classes.stepper}>
+                <Stepper activeStep={activeStep} className={classes.stepper}>
                     {steps.map((step) => (
                         <Step key={step}>
                             <StepLabel>{step}</StepLabel>
