f7c3a56a5103a67c523de6a274c4d4c0a6f6c7ee
bishalr0y <bishalroy895@gmail.com>
2021-06-11 11:39:11 +0530

    remove stripe

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Typography, Button, Divider, Card } from '@material-ui/core';
+import { Typography, Button, Divider } from '@material-ui/core';
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+
 import Review from './Review';
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
 
 const PaymentForm = ({ checkoutToken, shippingData, nextStep, backStep, onCaptureCheckout }) => {
 
-    const handleSubmit = async (event, elements, stripe) => {
+    const handleSubmit = async (event) => {
         
         event.preventDefault(); //this will prevent the website from getting refresh after the button is clicked
-        if(!stripe || !elements) return; //stripe cannot do anything if we don't have these
-        const cardElement = elements.getElement(CardElement);
+       
 
-        const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 
-        'card', card: cardElement });
+       
 
-        if(error) {
-            console.log("[Error]", error);
-        } else {
+        
             const orderData = {
                 line_items: checkoutToken.live.line_items,
                 customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email }, 
@@ -30,52 +26,31 @@ const PaymentForm = ({ checkoutToken, shippingData, nextStep, backStep, onCaptur
                     county_state: shippingData.shippingSubdivision,
                     postal_zip_code: shippingData.zip,
                     country: shippingData.shippingCountry
-                },
-                fulfillment: { shipping_method: shippingData.shippingOption }, 
-                payment: {
-                    gateaway: 'stripe',
-                    stripe: {
-                        payment_method_id: paymentMethod.id,
-                    }, 
-
-                },
+                }
             };
-            onCaptureCheckout(checkoutToken.id, orderData);
+        onCaptureCheckout(checkoutToken.id, orderData);
 
             nextStep();
-        }
     };
 
    
 
     return (
-        <>
-            <Review checkoutToken={checkoutToken} />
-            <Divider />
-            <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment Method</Typography>
-            <Elements stripe={stripePromise}>
-                <ElementsConsumer>
-                    {({ elements, stripe }) => (
-                        <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
-                            <CardElement />
-                            <br />
-                            <br />
-                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                <Button variant="outlined" onClick={backStep}>Back</Button>
-                                <Button type="submit" variant="contained" disabled={!stripe} color="primary">
-                                    Pay {checkoutToken.live.subtotal.formatted_with_symbol}
-                                </Button>
-                            </div>
-                        </form>
-                    )}
-                </ElementsConsumer>
-            </Elements>
-        </>
+        <div>
+        <Review checkoutToken = {checkoutToken} />
+        <Divider />
+        <Typography varinat ="h6" gutterBottom style = {{margin: '20px 0'}}>Payment Method</Typography>
+            <div style = {{display: 'flex', justifyContent: 'space-between'}}>
+                <Button variant = "outlined" onClick = {backStep} > Back </Button>
+                <Button type = "submit" variant = "contained" color= "primary" onClick = {(e) => handleSubmit(e)}> 
+                            Pay {checkoutToken.live.subtotal.formatted_with_symbol} 
+                </Button>
+            </div>
+    </div>
     )
 }
 
 export default PaymentForm;
-
 
 
 
